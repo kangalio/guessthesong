8b62683fd5e06f8a83abdeae33621b11b7c4cad0
kangalioo <jannik.a.schaper@web.de>
2023-03-23 18:32:13 +0100

    More room data saved

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-struct Room {
+struct Player {
     name: String,
 }
 
+struct Room {
+    name: String,
+    code: u32,
+    players: Vec<Player>,
+    password: Option<String>, // If None, room is public
+    explicit_songs: bool,
+    num_rounds: u32,
+    round_time_secs: u32,
+    created_at: std::time::Instant,
+}
+
 struct State {
-    rooms: std::sync::Mutex<Vec<Room>>,
+    rooms: parking_lot::Mutex<Vec<Room>>,
 }
 
 fn http_url_to_local_path(url: &str) -> std::path::PathBuf {
@@ -22,24 +33,55 @@ fn create_room(state: &State, body: &str) {
     let mut params = std::collections::HashMap::new();
     for kv_pair in body.split('&') {
         let Some((key, value)) = kv_pair.split_once('=') else {
-            println!("Error: invalid kv pair: {}", kv_pair);
+            log::error!("invalid kv pair: {}", kv_pair);
             continue;
         };
 
         params.insert(key, value);
     }
 
-    state.rooms.lock().unwrap().push(Room {
-        name: params.get("room_name").copied().unwrap_or("").to_string(),
-    });
+    {
+        let mut rooms = state.rooms.lock();
+        let code = rooms.iter().map(|r| r.code).max().unwrap_or(0) + 1; // generate a new unambiguous code
+        rooms.push(Room {
+            name: params.get("room_name").copied().unwrap_or("").to_string(),
+            code,
+            password: match params.get("password").copied() {
+                None | Some("") => None,
+                Some(x) => Some(x.to_string()),
+            },
+            players: vec![Player {
+                name: params.get("username").copied().unwrap_or("").to_string(),
+            }],
+            explicit_songs: params.get("explicit").copied() == Some("y"),
+            num_rounds: params
+                .get("rounds")
+                .and_then(|x| x.parse().ok())
+                .unwrap_or(9),
+            round_time_secs: params
+                .get("round_time")
+                .and_then(|x| x.parse().ok())
+                .unwrap_or(75),
+            created_at: std::time::Instant::now(),
+        });
+    }
 }
 
 fn main() {
+    env_logger::init();
+
     let server = tiny_http::Server::http("0.0.0.0:5234").expect("failed to open HTTP server");
 
     let state = std::sync::Arc::new(State {
-        rooms: std::sync::Mutex::new(vec![Room {
+        rooms: parking_lot::Mutex::new(vec![Room {
+            code: 420,
             name: "starter room lol".to_string(),
+            players: vec![],
+            password: None,
+            explicit_songs: true,
+            num_rounds: 9,
+            round_time_secs: 75,
+            created_at: std::time::Instant::now(),
         }]),
     });
 
@@ -49,25 +91,37 @@ fn main() {
 
         let server = std::net::TcpListener::bind("0.0.0.0:9001").unwrap();
         for stream in server.incoming() {
+            let mut websocket = match stream {
+                Ok(stream) => match tungstenite::accept(stream) {
+                    Ok(websocket) => websocket,
+                    Err(e) => {
+                        log::error!("websocket connection failed: {}", e);
+                        continue;
+                    }
+                },
+                Err(e) => {
+                    log::error!("websocket connection failed: {}", e);
+                    continue;
+                }
+            };
+
             let state2 = state.clone();
             std::thread::spawn(move || {
                 let state = state2;
-                let mut websocket = tungstenite::accept(stream.unwrap()).unwrap();
 
                 loop {
                     let rooms = state
                         .rooms
                         .lock()
-                        .unwrap()
                         .iter()
                         .map(|r| {
                             serde_json::json!( {
-                                "code": 1080,
+                                "code": r.code,
                                 "game_mode": "Themes",
-                                "idle": 50,
+                                "idle": (std::time::Instant::now() - r.created_at).as_secs(),
                                 "name": &r.name,
-                                "players": 4,
-                                "status": "Public",
+                                "players": r.players.len(),
+                                "status": if r.password.is_some() { "Private" } else { "Public" },
                                 "theme": "Random songs",
                             } )
                         })
@@ -76,11 +130,16 @@ fn main() {
                         "state": "fetch_new",
                         "msg": rooms,
                     } );
-                    websocket
-                        .write_message(tungstenite::Message::Text(
-                            serde_json::to_string(&msg).unwrap(),
-                        ))
-                        .unwrap();
+
+                    if let Err(e) = websocket.write_message(tungstenite::Message::Text(
+                        serde_json::to_string(&msg).expect("can't happen"),
+                    )) {
+                        log::info!(
+                            "Failed to send room data, stopping WebSocket connection: {}",
+                            e
+                        );
+                        break;
+                    }
 
                     std::thread::sleep(std::time::Duration::from_secs(5));
                 }
@@ -92,13 +151,13 @@ fn main() {
         let mut request = match server.recv() {
             Ok(x) => x,
             Err(e) => {
-                println!("Error: failed to receive HTTP request: {}", e);
+                log::error!("failed to receive HTTP request: {}", e);
                 continue;
             }
         };
         let mut body = String::new();
         if let Err(e) = request.as_reader().read_to_string(&mut body) {
-            println!("Error. failed to read request body: {}", e);
+            log::error!("failed to read request body: {}", e);
         }
 
         match request.method() {
@@ -112,7 +171,7 @@ fn main() {
                         ),
                     };
                 if let Err(e) = response_result {
-                    println!("Error: failed to send HTTP response: {}", e);
+                    log::error!("failed to send HTTP response: {}", e);
                 }
             }
             tiny_http::Method::Post => {
@@ -121,7 +180,7 @@ fn main() {
                 }
             }
             other => {
-                println!("Error: unknown HTTP method: {}", other);
+                log::error!("unknown HTTP method: {}", other);
             }
         }
     }

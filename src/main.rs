b6ca036668b76bdad2229776b49380e1b7060e73
kangalioo <jannik.a.schaper@web.de>
2023-05-23 18:32:48 +0200

    Make Spotify token not expire after 1 hour
    https://github.com/ramsayleung/rspotify/issues/413

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,18 @@ async fn main() {
     dotenv::dotenv().ok();
     env_logger::init();
 
-    let spotify_client = rspotify::ClientCredsSpotify::new(rspotify::Credentials {
+    // Setup Spotify client
+    let creds = rspotify::Credentials {
         id: std::env::var("SPOTIFY_ID").expect("missing SPOTIFY_ID env variable"),
         secret: Some(std::env::var("SPOTIFY_SECRET").expect("missing SPOTIFY_SECRET env variable")),
-    });
+    };
+    let config = rspotify::Config {
+        token_refreshing: true, // not enabled by default for ??? reasons
+        ..Default::default()
+    };
+    let spotify_client = rspotify::ClientCredsSpotify::with_config(creds, config);
     spotify_client.request_token().await.expect("invalid Spotify credentials");
 
+    // Run HTTP server
     routes::run_axum(spotify_client).await;
 }
